dbcb6984ca15c3d85fbfdce11004453538bdc5c4
Nicholas Kaldis <nicholas.kaldis@student.tdsb.on.ca>
2022-06-21 21:58:46 -0400

    Beginning of Menu, code complete and fixed

--- rock_paper_scissors_advanced.py
+++ rock_paper_scissors_advanced.py
@@ -4,59 +4,72 @@ plays = ["ROCK","PAPER","SCISSORS"]
 humanWins = [-2, 1]
 humanCount = 0
 computerCount = 0
-humanChoice = False
-playContinue = True
 
 
 def getValidHumanPlay( low, high ):
+        humanChoice = False
 
         while humanChoice == False:
-                
-                humanInput = int(input("Enter 0 for Rock, 1 for Paper, 2 for Scissors"))
-	
-                if humanInput >= low and humanInput <= high:
+
+                humanInput = int(input("Enter 0 for Rock, 1 for Paper, 2 for Scissors: "))
+
+                if humanInput <= high and humanInput >= low:
                         humanChoice = True
                 else:
                         humanChoice = False
-	return
+                        
+        return humanInput
 
 def getPlay( human, plays ):
-        
+        playContinue = True
         while playContinue == True:
                 humanPlay = getValidHumanPlay(0,2)
                 computerPlay = random.randint(0,2)
-                print("Human chose ",humanPlay)
-                print("Computer chose ",computerPlay)
+
+                print(human,"chose ",plays[humanPlay])
+                print("Computer chose ",plays[computerPlay])
                 if humanPlay == computerPlay:
-                        PlayContinue = True
-                else:
                         playContinue = False
-        
-	return 
+                        break
+                break
+        return humanPlay,computerPlay
+
 
 def checkWinner( play, plays, humanCount, computerCount ):
-        result = play[0]-play[1]
+        result = play[0] - play[1]
         if result in humanWins:
                 humanCount += 1
-                print("Human won with",plays(humanPlay),"beating",plays(computerPlay)
+                print(human,"won with",plays[play[0]],"beating",plays[play[1]])
+        
+        elif play[0] == play[1]:
+                print ("It is a tie")
+                
         else:
                 computerCount += 1
-                print("Computer won with",plays(computerPlay),"beating",plays(humanPlay)
+                print("Computer won with",plays[play[1]],"beating",plays[play[0]])
 
-	return 
+        return humanCount, computerCount
 
 
-### Beginning of Main
 
 human = input( "Enter your name ").upper()
 
 keepPlaying = True
 
 while keepPlaying:
-	#create a variable called play and call the getPlay function, sending human and plays variable
-	#using humanCount and computerCount, call the checkWinner function and send the variables: play, plays, humanCount, computerCount as parameters
-	#using the keepPlaying variable do a conditional statement check that computerCount is less than 3 and humanCount is less than 3
+        play = getPlay(human, plays)
+        scoreList = checkWinner( play, plays, humanCount, computerCount )
+        humanCount = scoreList[0]
+        computerCount = scoreList[1]
 
-#create an if-statement (not inside the while loop) that checks if the human has won 3 times
-	#if the human has won 3 times declare that they have won the match and display humanCount to computerCount
-#else if the computer has won, declare they have won the match and display computerCount to humanCount
+        if computerCount < 3 and humanCount < 3:
+                keepPlaying = True
+        else:
+                keepPlaying = False
+if humanCount == 3:
+        print(human,"has won the match",humanCount,"-",computerCount)
+else:
+                
+        print("Computer has won the match",computerCount,"-",humanCount)
+
+
